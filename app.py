784a6c70f7c553bfced5d1baf259de2e93542725
Thirumal <m.thirumal@hotmail.com>
2022-05-20 09:29:19 +0530

    UTC & IST time

--- app.py
+++ app.py
@@ -8,24 +8,29 @@ app = Chalice(app_name='aws-service-start-stop-scheduler')
 rds = boto3.client('rds')
 
 
-# Run at 1:30pm (UTC) (i.e 07:00pm [IST]) every Monday through Friday.
-@app.schedule(Cron(30, 13, '?', '*', 'MON-FRI', '*'))
-def stop_lambda_handler(event):
-    print("Stopping cluster")
-    response = rds.stop_db_cluster(DBClusterIdentifier=os.environ.get("DBClusterIdentifier"))
-    print('Stopped your cluster: ' + str(response))
-    send_notification_to_ms_teams("Stop cluster response {}".format(response))
-
-
+# -----------------------START SERVICE-------------------------------#
 # Run at 02:30am(UTC) [i.e.08:00am (IST)] every Monday through Friday.
 @app.schedule(Cron(30, 2, '?', '*', 'MON-FRI', '*'))
 def start_lambda_handler(event):
     print("Starting cluster")
     response = rds.start_db_cluster(DBClusterIdentifier=os.environ.get("DBClusterIdentifier"))
     print('Started your cluster: ' + str(response))
+    # Sending notification to Microsoft Team
     send_notification_to_ms_teams("Start cluster response {}".format(response))
 
 
+# -----------------------STOP SERVICE-------------------------------#
+# Run at 1:30pm (UTC) (i.e 07:00pm [IST]) every Monday through Friday.
+@app.schedule(Cron(30, 13, '?', '*', 'MON-FRI', '*'))
+def stop_lambda_handler(event):
+    print("Stopping cluster")
+    response = rds.stop_db_cluster(DBClusterIdentifier=os.environ.get("DBClusterIdentifier"))
+    print('Stopped your cluster: ' + str(response))
+    # Sending notification to Microsoft Team
+    send_notification_to_ms_teams("Stop cluster response {}".format(response))
+
+
+# Sending notification to Microsoft Team
 def send_notification_to_ms_teams(message):
     print("Sending message {} to MS Teams".format(message))
     ms_teams_message = pymsteams.connectorcard(os.environ.get('msTeamChannelIncomingWebhook'))
