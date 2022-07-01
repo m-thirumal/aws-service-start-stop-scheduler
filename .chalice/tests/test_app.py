07b099a020b80cdc83951b22262a51430e3ef149
Thirumal <m.thirumal@hotmail.com>
2022-07-01 19:48:24 +0530

    Added json for testing

---
+++ .chalice/tests/test_app.py
@@ -0,0 +1,32 @@
+from app import app
+from chalice.test import Client
+import pytest
+import json
+import logging
+
+
+@pytest.mark.skip
+def test_pass():
+    assert 0 == 0
+
+
+@pytest.mark.skip
+def test_start_neptune():
+    logging.debug('Test start neptune')
+    with open('tests/start_neptune.json', ) as f:
+        data = json.load(f)
+    with Client(app, stage_name='prod') as client:
+        result = client.lambda_.invoke('handler', data)
+        logging.debug("result %s", result.payload)
+        assert result.payload is not None
+
+
+@pytest.mark.skip
+def test_stop_neptune():
+    logging.debug('Test start neptune')
+    with open('tests/start_neptune.json', ) as f:
+        data = json.load(f)
+    with Client(app, stage_name='prod') as client:
+        result = client.lambda_.invoke('handler', data)
+        logging.debug("result %s", result.payload)
+        assert result.payload is not None
